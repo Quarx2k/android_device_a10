076877318b4d9a66f15e2f789fed120be6d3cb32
Quarx2k <agent00791@gmail.com>
2012-05-03 12:12:10 +0700

    fix build conflict

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -30,7 +30,7 @@ LOCAL_SRC_FILES := AudioPolicyManager.cpp
 LOCAL_SHARED_LIBRARIES := libcutils libutils libmedia
 LOCAL_STATIC_LIBRARIES := libmedia_helper
 LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy
-LOCAL_MODULE := audio_policy.default
+LOCAL_MODULE := audio_policy.exDroid
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
