02c708bcd963804c5ae7e87720c8c4dfc32cc840
Quarx2k <agent00791@gmail.com>
2012-05-28 17:09:12 +0700

    update boot.img, fix typo in bootanimation name

--- cm.mk
+++ cm.mk
@@ -19,7 +19,7 @@ $(call inherit-product, device/allwinner/a10/full_a10.mk)
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, device/allwinner/a10/a10-blobs.mk)
 
-TARGET_BOOTANIMATION_NAME :=  vertical-1024x600
+TARGET_BOOTANIMATION_NAME := horizontal-1024x600
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_a10
