ae4b3394c34125be80ca68beae6fd81a27cdbced
Quarx2k <agent00791@gmail.com>
2012-05-31 13:22:46 +0700

    fix rild conflict build

--- rild/Android.mk
+++ rild/Android.mk
@@ -16,24 +16,10 @@ LOCAL_SHARED_LIBRARIES += libdl
 endif # arm
 
 LOCAL_CFLAGS := -DRIL_SHLIB
+LOCAL_MODULE:= rild_sun4i
 
-LOCAL_MODULE:= rild
+LOCAL_MODULE_STEM := rild
 
-include $(BUILD_EXECUTABLE)
-
-# For radiooptions binary
-# =======================
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-	radiooptions.c
-
-LOCAL_SHARED_LIBRARIES := \
-	libcutils \
-
-LOCAL_CFLAGS := \
-
-LOCAL_MODULE:= radiooptions
-LOCAL_MODULE_TAGS := debug
 
 include $(BUILD_EXECUTABLE)
+
