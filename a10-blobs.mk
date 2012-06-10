8b77fcc01feb15fdb88a0e13361725d3527edc1a
Quarx2k <agent00791@gmail.com>
2012-06-10 14:33:21 +0700

    fix update-script, fix build

--- a10-blobs.mk
+++ a10-blobs.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+device_path = device/allwinner/a10
+
 DEVICE_PREBUILT := ${device_path}/prebuilt
 
 # EGL Stuff
