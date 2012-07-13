ead6bee0a889af69ecd34964895b4adb857e21de
Emilio López <turl@tuxfamily.org>
2012-07-13 00:46:02 +0000

    lights: rework based on tuna source
    There's something fishy causing memory corruption, and it's easier
    to rewrite it based on tuna rather than debugging it.
    
    Conflicts:
    
    	liblights/lights.c

--- liblights/lights.c
+++ liblights/lights.c
@@ -13,157 +13,119 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_NDEBUG 0
+
+//#define LOG_NDEBUG 0
 #define LOG_TAG "lights"
-
 #include <cutils/log.h>
-
 #include <stdint.h>
 #include <string.h>
-#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
-
 #include <sys/ioctl.h>
 #include <sys/types.h>
-
 #include <hardware/lights.h>
 #include <drv_display_sun4i.h>
 
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
-static int g_backlight = 14;
-
-/* Only one instance is created per platform */
-struct light_context_t 
+void init_g_lock(void)
 {
-    struct light_device_t     device;
-    int                       fd;
-};
-
-/* device methods */
-void init_globals(void)
-{
-    pthread_mutex_init(&g_lock, NULL);
-} 
+	pthread_mutex_init(&g_lock, NULL);
+}
 
 static int
 rgb_to_brightness(struct light_state_t const* state)
 {
     int color = state->color & 0x00ffffff;
     int bright;
-    
+
     bright = ((77*((color>>16)&0x00ff)) + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
+
+    /* fix bright value >=5 , for HW reason */
+    if(bright < 5)
+        bright = 5;
 
     return bright;
 }
 
-static int
-set_light_backlight(struct light_device_t* dev,
-        struct light_state_t const* state)
+static int set_light_backlight(struct light_device_t *dev,
+			struct light_state_t const *state)
 {
-    struct light_context_t      *ctx;
+	int err = 0;
+	int fd;
+	int brightness = rgb_to_brightness(state);
 
-    int err = 0;
+	unsigned long  args[3];
+	args[0] = 0;
+	args[1] = brightness;
+	args[2] = 0;
 
-    int brightness = rgb_to_brightness(state);
-	
-    pthread_mutex_lock(&g_lock);	
-    g_backlight = brightness;
-    ctx = (struct light_context_t *)dev;
-    unsigned long  args[3];
+	pthread_mutex_lock(&g_lock);
 
-	args[0]  = 0;
-	args[1]  = brightness;
-	args[2]  = 0;
-	err = ioctl(ctx->fd,DISP_CMD_LCD_SET_BRIGHTNESS,args);
-	
-	if(err == 0)
+	fd = open("/dev/disp", O_RDONLY);
+	if (fd < 0)
 	{
-		g_backlight = brightness;
+		ALOGE("Failed to open display device fd = %x\n", fd);
+		pthread_mutex_unlock(&g_lock);
+		return -1;
 	}
 
-    pthread_mutex_unlock(&g_lock);
-    
-    return err;
+	err = ioctl(fd, DISP_CMD_LCD_SET_BRIGHTNESS, args);
+
+	close(fd);
+
+	pthread_mutex_unlock(&g_lock);
+	return err;
 }
 
-/** Close the lights device */
-static int
-close_lights(struct light_device_t *dev)
+static int close_lights(struct light_device_t *dev)
 {
-    struct light_context_t *ctx;
+	ALOGV("close_light is called");
+	if (dev)
+		free(dev);
 
-    ctx = (struct light_context_t *)dev;
-    if(ctx->fd)
-    {
-        close(ctx->fd);
-    }
-    if (dev) {
-        free(dev);
-    }
-    return 0;
+	return 0;
 }
 
-/** Open a new instance of a lights device using name */
-static int open_lights(const struct hw_module_t* module, char const* name,
-        struct hw_device_t** device)
+static int open_lights(const struct hw_module_t *module, char const *name,
+						struct hw_device_t **device)
 {
-    int (*set_light)(struct light_device_t* dev,
-            struct light_state_t const* state);
+	int (*set_light)(struct light_device_t *dev,
+		struct light_state_t const *state);
 
-    if (0 == strcmp(LIGHT_ID_BACKLIGHT, name)) {
-        set_light = set_light_backlight;
-    }
-    else {
-        return -EINVAL;
-    }
+	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
+		set_light = set_light_backlight;
+	else
+		return -EINVAL;
 
-    pthread_once(&g_init, init_globals);
+	pthread_once(&g_init, init_g_lock);
 
-    struct light_context_t *dev = (struct light_context_t *)malloc(sizeof(struct light_context_t));
-    memset(dev, 0, sizeof(struct light_context_t));
+	struct light_device_t *dev = malloc(sizeof(struct light_device_t));
+	memset(dev, 0, sizeof(*dev));
 
-    ALOGD("light set backlight! name = %s\n", name);
+	dev->common.tag = HARDWARE_DEVICE_TAG;
+	dev->common.version = 0;
+	dev->common.module = (struct hw_module_t *)module;
+	dev->common.close = (int (*)(struct hw_device_t *))close_lights;
+	dev->set_light = set_light;
 
-    if(0 == strcmp(LIGHT_ID_BACKLIGHT, name)) {
-        dev->fd = open("/dev/disp", O_RDONLY);
-        if (dev->fd < 0) {
-            ALOGE("Failed to open display device dev->fd = %x\n", dev->fd);
-        }
-    }
-    
-    dev->device.common.tag = HARDWARE_DEVICE_TAG;
-    dev->device.common.version = 0;
-    dev->device.common.module = (struct hw_module_t*)module;
-    dev->device.common.close = (int (*)(struct hw_device_t*))close_lights;
-    dev->device.set_light = set_light;
+	*device = (struct hw_device_t *)dev;
 
-    *device = (struct hw_device_t*)dev;
-    
-    return 0;
-
-error:
-    free(dev);
-
-    return -EINVAL;
+	return 0;
 }
 
 static struct hw_module_methods_t lights_module_methods = {
-    .open =  open_lights,
+	.open =  open_lights,
 };
 
-/*
- * The lights Module
- */
-const struct hw_module_t HAL_MODULE_INFO_SYM = {
-    .tag = HARDWARE_MODULE_TAG,
-    .version_major = 1,
-    .version_minor = 0,
-    .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "A10 lights Module",
-    .author = "SoftWinner,Quarx",
-    .methods = &lights_module_methods,
+struct hw_module_t HAL_MODULE_INFO_SYM = {
+	.tag = HARDWARE_MODULE_TAG,
+	.version_major = 1,
+	.version_minor = 0,
+	.id = LIGHTS_HARDWARE_MODULE_ID,
+	.name = "lights Module",
+	.author = "The CyanogenMod Project",
+	.methods = &lights_module_methods,
 };
